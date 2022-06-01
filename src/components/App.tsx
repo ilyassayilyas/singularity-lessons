ad3ccc8f204e067a6aa7483068e5b0902e728a52
Ilyas.Bekpayev <Ilyas.Bekpayev@IMAC-3-6.local>
2022-06-01 17:14:31 +0600

    upload dnd

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./Board";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Board />
+      </div>
+    </DndProvider>
+  );
+}
+
+export default App;
